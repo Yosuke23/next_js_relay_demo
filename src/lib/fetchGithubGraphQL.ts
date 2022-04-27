fa071126df259b626bcc7ea97cdb0a0b56f67627
y-narumi <yosuke-narumi@p2.techfirm.co.jp>
2022-04-27 13:03:29 +0900

    first-commit

---
+++ src/lib/fetchGithubGraphQL.ts
@@ -0,0 +1,19 @@
+const fetchGithubGraphQL = async(text: string, variables: any) => {
+    const Token = process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN;
+    console.log(Token);
+  const response = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `bearer ${Token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  return await response.json();
+}
+
+export default fetchGithubGraphQL;
