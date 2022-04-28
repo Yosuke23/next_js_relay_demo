a838872f30a3681d99faa8efab235b04f78a4637
y-narumi <yosuke-narumi@p2.techfirm.co.jp>
2022-04-28 16:29:13 +0900

    Update demo.tsx

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { fetchQuery } from 'react-relay'
+import { fetchQuery } from 'react-relay/hooks';
 import initEnvironment from '../lib/relay'
-import {repository} from '../queries/repository'
+import repository from '../queries/repository'
+import fetchFunction from '../lib/fetchFunction';
+import Link from 'next/link';
+import moment from 'moment';
+import {
+    Environment,
+    Network,
+    RecordSource,
+    Store,
+    RequestParameters,
+    Variables, // 型のimport
+    CacheConfig, // 型のimport
+    UploadableMap, // 型のimport
+    GraphQLResponse, // 型のimport
+    QueryResponseCache, // 型のimport
+} from 'relay-runtime';
+import Header from '../components/atoms/Header'
+import Footer from '../components/atoms/Footer';
 
 export async function getServerSideProps() {
-  const environment: any = initEnvironment
+    const environment: any = new Environment({
+    network: Network.create(fetchFunction),
+    store: new Store(new RecordSource)
+  })
+  console.log(environment)
+  console.log(repository)
   const queryProps: any = await fetchQuery(environment, repository, {}).toPromise()
+
   return {
     props: {
       ...queryProps,
@@ -13,8 +36,34 @@ export async function getServerSideProps() {
   }
 }
 
-export default function Home(props: any) {
+export default function Home(props: any, id: string) {
+  console.log(props.repository.name)
+  console.log(props.user.name)
   return (
-    <p>{props != null ? `Repository: ${props.repository.name}` : "Loading"}</p>
+    <>
+      <Header avatarImagePath={props != null ? `${props.user.avatarUrl}` : ""} />
+        <main className="text-center">
+          <div className="mt-10 ml-10 overflow-hidden rounded-lg w-11/12 h-96 bg-gray-50 border border-gray-200">
+            <h1 className='text-xl font-bold text-gray-700 mt-12'>
+            My GitHub Personal Data...
+            </h1>
+            <div className=" ml-32">
+          </div>
+          <div className="mt-10">
+            <span className="text-gray-700">
+              Repository: 
+            </span>
+            <Link href={props != null ? `${props.repository.url}` : "Loading"}  passHref >
+              <span className="text-blue-600">
+              {props != null ? ` ${props.repository.name}` : "Loading"}
+              </span>
+            </Link>
+          </div>       
+            <p className="text-gray-700 flex-grow">{props != null ? `name: ${props.user.name}` : "Loading"}({props != null ? `since: ${moment(props.user.createdAt).format('YYYY-MM-DD(dddd)')}` : "Loading"}</p>
+            <p className="text-gray-700">{props != null ? `created at: ${ moment(props.repository.createdAt).format('YYYY-MM-DD(dddd)')}` : "Loading"}</p>
+          </div>
+        </main>
+      <Footer/>
+    </>
   )
 }
