d53fbbbfce37cffaf0f35ca619a649f31be3d940
y-narumi <yosuke-narumi@p2.techfirm.co.jp>
2022-04-28 17:00:52 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css';
-import '@tailwindcss/forms';
 import { ReactRelayContext } from 'react-relay'
 import useEnvironment from '../lib/relay'
+import 'tailwindcss/tailwind.css';
+import '@tailwindcss/forms';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const environment = useEnvironment(pageProps)
